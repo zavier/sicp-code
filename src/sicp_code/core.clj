50430f8be648b5ca4c4c91b61b375306acd3e78d
zheng <765324639@qq.com>
2022-01-22 11:26:03 +0800

    Initial commit

---
+++ src/sicp_code/core.clj
@@ -0,0 +1,11 @@
+(ns sicp-code.core
+  (:gen-class))
+
+
+
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println (Math/sqrt 9)))
+
---
+++ src/sicp_code/sqrt.clj
@@ -0,0 +1,25 @@
+(ns sicp-code.sqrt)
+
+(defn average [x y]
+  (/ (+ x y) 2))
+
+(defn improve [guess x]
+  (average guess (/ x guess)))
+
+(defn good-enough? [guess x]
+  (< (Math/abs (- (* guess guess) x)) 0.001))
+
+(defn sqrt-iter [guess x]
+  (if (good-enough? guess x)
+    guess
+    (recur (improve guess x) x)))
+
+(defn sqrt [x]
+  "计费平方根"
+  (sqrt-iter 1.0 x))
+
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println (sqrt 1000)))
